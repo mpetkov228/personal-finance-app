195864b24c7b57f45748585bee50ce01a52523dd
MihailPetkov96 <mpetkov228@gmail.com>
2026-03-08 20:48:05 +0200

    Add buttons to sidebar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,32 @@
+import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [screen, setScreen] = useState('dashboard');
+
   return (
     <>
       {/* Sidebar */}
       <div>
-        <div>Ledger</div>
-        <div>Personal Finance</div>
+        <div>
+          <div>Wealth Tracker</div>
+          <div>Personal Finance</div>
+        </div>
+
+        <button>Dashboard</button>
+        <button>Transactions</button>
+        <button>Budgets</button>
+
+        <div>
+          <div>Net Balance</div>
+          <div>+$2,415</div>
+        </div>
       </div>
+
+      {/* Main */}
+      <main>
+        {screen === 'dashboard' && <div>Dashboard</div>}
+      </main>
     </>
   )
 }
